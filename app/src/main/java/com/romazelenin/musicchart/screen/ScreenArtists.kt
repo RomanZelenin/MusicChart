550e624c3e824e62b294b7aeb23ca8156c7c3e1a
Roman Zelenin <roman_zelenin@hotmail.com>
2022-04-04 11:36:31 +0300

    Added color for different ratings

--- app/src/main/java/com/romazelenin/musicchart/screen/ScreenArtists.kt
+++ app/src/main/java/com/romazelenin/musicchart/screen/ScreenArtists.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -76,16 +76,19 @@ fun ListArtists(navController: NavController, artists: LazyPagingItems<Artist>) 
                     .padding(8.dp)
                     .clickable {
                         navController.navigate("albums/${artist.artist_name}/${artist.artist_id}")
-                    }) {
+                    },
+                verticalAlignment = Alignment.Top) {
+                    Text(text = "${index+1} ")
                     Text(
-                        text = "${index + 1} ${artist.artist_name}",
+                        text = artist.artist_name,
                         modifier = Modifier
                             .weight(1f)
                     )
+                    Spacer(modifier = Modifier.width(8.dp))
                     Box(
                         modifier = Modifier
-                            .clip(CircleShape)
-                            .background(Color.LightGray)
+                            .clip(RoundedCornerShape(16.dp))
+                            .background(getColorForRating(artist.artist_rating))
                             .size(50.dp)
                             .padding(8.dp),
                         contentAlignment = Alignment.Center
@@ -99,6 +102,15 @@ fun ListArtists(navController: NavController, artists: LazyPagingItems<Artist>) 
         if (artists.loadState.append is LoadState.Loading) {
             item { CircularProgressIndicator(Modifier.padding(16.dp)) }
         }
+    }
+}
+
+private fun getColorForRating(rating: Int): Color {
+   return when (rating) {
+        in 70..100 -> Color.Green
+        in 50..69 -> Color.Yellow
+        in 30..49-> Color.Red
+        else-> Color.LightGray
     }
 }
 
